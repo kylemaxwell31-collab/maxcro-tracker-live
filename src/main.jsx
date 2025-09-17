d56fab332ea66d991c38f79c25171ddb338c2e74
kylemaxwell31-collab <kylemaxwell31@gmail.com>
2025-09-17 17:47:57 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-white">
+        Maxcro Tracker
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
